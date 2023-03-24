a6415d2b93781a86a1ee720f0a7446b5e31c7d01
Wendril-Costa <wendrild.c.n@gmail.com>
2023-03-24 20:04:52 -0300

    Requisito 3 Feito - Retornando os valores de industry

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,22 +1,10 @@
 from typing import List, Dict
+from src.insights.jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+    return list(set(jobs['industry']
+                for jobs in read(path) if jobs['industry']))
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
