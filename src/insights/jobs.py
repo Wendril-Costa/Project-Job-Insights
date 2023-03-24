a010d88b730c646e72f9169779a4f476263f6248
Wendril-Costa <wendrild.c.n@gmail.com>
2023-03-24 19:22:39 -0300

    Requisito 2 Feito - jobs_types extraido com sucesso do arquivo csv

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -5,26 +5,15 @@ import csv
 
 @lru_cache
 def read(path: str) -> List[Dict]:
-    with open(path) as jobs_path_file:
-        return [jobs for jobs in csv.DictReader(jobs_path_file)]
+    try:
+        with open(path) as jobs_path_file:
+            return [jobs for jobs in csv.DictReader(jobs_path_file)]
+    except FileNotFoundError:
+        print('Aquivo não foi encontrado')
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    raise NotImplementedError
+    return list(set(jobs['job_type'] for jobs in read(path)))
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
