9ab1a8efe2d18bee5c4bde2fd45b2bfd724a2d85
Wendril-Costa <wendrild.c.n@gmail.com>
2023-04-03 22:40:17 -0300

    Requisito 12 Feito Test para sorting

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,12 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
+from src.insights.jobs import read
 
 
 def test_sort_by_criteria():
-    pass
+    sort_by(read("data/jobs.csv"), "max_salary")
+
+    assert read("data/jobs.csv")[0]["min_salary"] == "195818"
+
+    assert read("data/jobs.csv")[0]["max_salary"] == "383416"
+
+    assert read("data/jobs.csv")[0]['date_posted'] == '2020-05-07'
