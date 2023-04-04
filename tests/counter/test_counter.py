927cff405ed715bbc1a7d7c77790bdf96a6626b0
Wendril-Costa <wendrild.c.n@gmail.com>
2023-04-03 21:30:25 -0300

    Requisito 10 Feito test para Counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,18 @@
-# from src.pre_built.counter import count_ocurrences
+import pytest
+from src.pre_built.counter import count_ocurrences
+from unittest.mock import mock_open, patch
 
 
-def test_counter():
-    pass
+@pytest.fixture
+def fixture():
+    """JavaScript JavaScript
+    Python Python Python C++ C C# C++ python javascript"""
+
+
+def test_counter(fixture):
+    assert count_ocurrences("data/jobs.csv", "javascript") == 122
+
+    assert count_ocurrences("data/jobs.csv", "python") == 1639
+
+    with patch("builtins.open", mock_open(read_data=fixture)):
+        assert count_ocurrences("data/jobs.csv", "JAVASCRIPT") == 3
